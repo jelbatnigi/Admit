1030753d2e5de47020b1547d0ca687aba61c3426
Jamal Elbatnigi <j_elbatn@Jamals-MacBook-Pro.local>
2017-01-23 17:31:43 -0500

    committing

--- src/main/resources/js/search.js
+++ src/main/resources/js/search.js
@@ -4,10 +4,14 @@ $(document).ready(function() {
      // alert($('#startId').val() + " " + $('#lastId').val());
      startId = $('#startId').val();
      lastId = $('#lastId').val();
+     if(startId > lastId)
+     {
+        alert("Start Event Id must be smaller than End Event Id");
+     }
     $.ajax({
         url: '/rest/searchEvents/'+startId+'/'+lastId
     }).then(function(data) {
-    table = '<br><br><table border="1""><tr><th>EventId</th><th>userId</th><th>NumberOdTIckets</th></tr>';
+    table = '<br><br><table class="table table-bordered"><tr><th>EventId</th><th>userId</th><th>NumberOfTIckets</th></tr>';
     rows = '';
     $.each(data, function() {
         rows += '<tr><td>' + this.purchaseId + '</td>' +
