cc9f515e36a41a7182c621c37ffa1a9ed4799393
Jamal Elbatnigi <j_elbatn@Jamals-MBP.home>
2017-01-23 19:31:33 -0500

    committing

---
+++ src/main/java/com/admitone/services/UserInfoService.java
@@ -0,0 +1,48 @@
+package com.admitone.services;
+
+import com.admitone.dao.UserInfoDAO;
+import com.admitone.model.UserInfo;
+import com.admitone.security.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Created by j_elbatn on 1/23/17.
+ */
+public class UserInfoService implements UserDetailsService {
+
+    @Autowired
+    UserInfoDAO userInfoDAO;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        UserInfo userInfo = null;
+        try {
+            userInfo = userInfoDAO.findUserByEmail(userName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if(userInfo == null)
+        {
+            throw new UsernameNotFoundException("User Not Found");
+        }
+        return new UserDetailsImpl(userInfo);
+
+    }
+
+    public UserInfo getUserById(long userId) throws Exception
+    {
+        UserInfo userInfo;
+        try {
+            userInfo = userInfoDAO.getUserInfoById(userId);
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+        return userInfo;
+
+    }
+
+}
